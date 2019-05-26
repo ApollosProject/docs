eaaa803ec73b95612918a47472ba4395e9e0459d
Michael Neeley <micneeley14@gmail.com>
2019-05-26 10:18:38 -0400

    some other stuff

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,7 +40,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: "install", label: "Docs" },
-    { doc: "doc2", label: "API" },
+    { doc: "button", label: "API" },
     { doc: "doc2", label: "Other" },
     { page: "help", label: "Help" },
     { blog: false }
