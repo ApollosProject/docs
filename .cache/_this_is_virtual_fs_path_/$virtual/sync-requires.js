5154a2fd9d472e7469ffeca604a6a7a23973f100
Richard Dubay <richardtdubayjr@gmail.com>
2022-01-12 08:17:46 -0500

    The very beginning of something magical

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/richdubay/Sites/docs/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/richdubay/Sites/docs/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/richdubay/Sites/docs/src/pages/index.js")),
+  "component---src-pages-landing-js": preferDefault(require("/Users/richdubay/Sites/docs/src/pages/landing.js"))
+}
+
