7cc21f28a96085213bcaadb43c14cae5ce75d637
Richard Dubay <richardtdubayjr@gmail.com>
2022-01-18 20:36:19 -0500

    Working on the pipeline section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import { ChalkboardTeacher, Code, GitBranch } from 'phosphor-react';
+import {
+  ChalkboardTeacher,
+  Code,
+  GitBranch,
+  RocketLaunch,
+} from 'phosphor-react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   const features = [
@@ -31,8 +37,44 @@ const IndexPage = () => {
       </Helmet>
       <div className='pipeline-wrapper'>
         <div className='pipeline'>
+          <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8'>
+            <div className='pipeline-green-background rounded-lg shadow-xl overflow-hidden'>
+              <div className='pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20'>
+                <div className='lg:self-center'>
+                  <p className='mt-6 max-w-3xl text-base text-base-secondary uppercase'>
+                    a Church-centric data pipeline
+                  </p>
+                  <h2 className='text-3xl font-extrabold text-white py-4'>
+                    <span className='block'>
+                      Syncs with your existing services and data.
+                    </span>
+                  </h2>
+                  <p className='mt-4 text-lg leading-6 text-dark-secondary'>
+                    Apollos syncs data with the other services you already
+                    depend on. Phasellus lorem quam molestie id quisque diam
+                    aenean nulla in. Accumsan in quis quis nunc, ullamcorper
+                    malesuada. Eleifend condimentum id viverra nulla.
+                  </p>
+                  <a
+                    href='#'
+                    className='mt-8 bg-white border border-transparent rounded-md shadow px-5 py-3 inline-flex gap-2 items-center text-base font-medium text-base-secondary hover:text-base-secondary-hover'
+                  >
+                    <RocketLaunch />
+                    Explore integrations
+                  </a>
+                </div>
+              </div>
+              <div className='-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1'>
+                <StaticImage
+                  className='transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20'
+                  src='../images/pipeline.png'
+                  alt='Pipeline Screenshot'
+                />
+              </div>
+            </div>
+          </div>
           <div className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-            <p className='mt-6 max-w-3xl text-base brand-text uppercase'>
+            <p className='mt-6 max-w-3xl text-base text-base-secondary uppercase'>
               Our Blueprint
             </p>
             <h1 className='text-5xl font-extrabold tracking-tight text-black'>
@@ -49,7 +91,7 @@ const IndexPage = () => {
                 {features.map((feature) => (
                   <div key={feature.name}>
                     <dt>
-                      <div className='flex items-center justify-center h-12 w-12 rounded-full blueprint-icon-bg brand-text'>
+                      <div className='flex items-center justify-center h-12 w-12 rounded-full blueprint-icon-bg text-base-secondary'>
                         <feature.icon className='h-6 w-6' aria-hidden='true' />
                       </div>
                       <p className='mt-5 text-lg leading-6 font-medium text-gray-900'>
@@ -78,9 +120,9 @@ const IndexPage = () => {
             </p>
             <button
               type='button'
-              className='footer-button inline-flex justify-center items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full sm:w-auto'
+              className='text-base-secondary inline-flex justify-center items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full sm:w-auto'
             >
-              Button text
+              Get started
             </button>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        base: {
+          secondary: '#17b582',
+        },
+        dark: {
+          secondary: '#f4f7f899',
+        },
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
