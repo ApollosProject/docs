bef9d445d9bfc1149dda24800415881991baacc2
Richard Dubay <richardtdubayjr@gmail.com>
2022-01-20 19:00:45 -0500

    Created the divider and cleaned up some spacing issues and started on the cards section

---
+++ src/components/Divider.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+const Divider = ({ icon }) => {
+  return (
+    <div className='relative'>
+      <div className='absolute inset-0 flex items-center' aria-hidden='true'>
+        <div className='w-full border-t border-gray-300' />
+      </div>
+      <div className='relative flex justify-center'>
+        <span className='bg-white px-2 text-gray-500'>
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M0 24L9.39372 0H14.6063L24 24H0ZM11.8571 5.8363L6.92808 19.4399H17.0847L12.1556 5.8363H11.8571Z'
+              fill='#00676D'
+            />
+          </svg>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Divider;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import {
   RocketLaunch,
 } from 'phosphor-react';
 import { StaticImage } from 'gatsby-plugin-image';
+import Divider from '../components/Divider';
 
 const IndexPage = () => {
   const features = [
@@ -35,26 +36,47 @@ const IndexPage = () => {
       <Helmet>
         <link rel='stylesheet' href='https://rsms.me/inter/inter.css'></link>
       </Helmet>
+      <div className='py-8 lg:py-24 pb-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
+        Heading
+        <div className='lg:columns-2'>
+          <div>Card 1</div>
+          <div>Card 2</div>
+          <div>Card 3</div>
+          <div>Card 4</div>
+          <div>Card 5</div>
+          <div>Card 6</div>
+        </div>
+      </div>
+      <div className='max-w-2xl mx-auto px-4'>
+        <Divider />
+      </div>
       <div className='pipeline-wrapper'>
-        <div className='pipeline'>
-          <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8'>
-            <div className='pipeline-green-background rounded-lg shadow-xl overflow-hidden'>
-              <div className='pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20'>
-                <div className='lg:self-center'>
-                  <p className='mt-6 max-w-3xl text-base text-base-secondary uppercase'>
-                    a Church-centric data pipeline
-                  </p>
-                  <h2 className='text-3xl font-extrabold text-white py-4'>
-                    <span className='block'>
-                      Syncs with your existing services and data.
-                    </span>
-                  </h2>
-                  <p className='mt-4 text-lg leading-6 text-dark-secondary'>
-                    Apollos syncs data with the other services you already
-                    depend on. Phasellus lorem quam molestie id quisque diam
-                    aenean nulla in. Accumsan in quis quis nunc, ullamcorper
-                    malesuada. Eleifend condimentum id viverra nulla.
-                  </p>
+        <div className='py-8 lg:py-24 pb-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+          <div className='pipeline-green-background rounded-lg shadow-xl overflow-hidden'>
+            <div className='pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20'>
+              <div className='lg:self-center'>
+                <p className='max-w-3xl text-sm text-base-secondary uppercase'>
+                  a Church-centric data pipeline
+                </p>
+                <h2 className='text-3xl font-extrabold text-white py-4'>
+                  <span className='block'>
+                    Syncs with your existing services and data.
+                  </span>
+                </h2>
+                <p className='mt-4 text-lg leading-6 text-dark-secondary'>
+                  Apollos syncs data with the other services you already depend
+                  on. Phasellus lorem quam molestie id quisque diam aenean nulla
+                  in. Accumsan in quis quis nunc, ullamcorper malesuada.
+                  Eleifend condimentum id viverra nulla.
+                </p>
+                <div className='flex flex-col items-center'>
+                  <div className='mt-14 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1'>
+                    <StaticImage
+                      className='transform rounded-md object-cover object-left-top'
+                      src='../images/pipeline.png'
+                      alt='Pipeline Screenshot'
+                    />
+                  </div>
                   <a
                     href='#'
                     className='mt-8 bg-white border border-transparent rounded-md shadow px-5 py-3 inline-flex gap-2 items-center text-base font-medium text-base-secondary hover:text-base-secondary-hover'
@@ -64,17 +86,10 @@ const IndexPage = () => {
                   </a>
                 </div>
               </div>
-              <div className='-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1'>
-                <StaticImage
-                  className='transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20'
-                  src='../images/pipeline.png'
-                  alt='Pipeline Screenshot'
-                />
-              </div>
             </div>
           </div>
-          <div className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-            <p className='mt-6 max-w-3xl text-base text-base-secondary uppercase'>
+          <div className='pt-8 lg:pt-24'>
+            <p className='max-w-3xl text-base text-base-secondary uppercase'>
               Our Blueprint
             </p>
             <h1 className='text-5xl font-extrabold tracking-tight text-black'>
@@ -83,9 +98,7 @@ const IndexPage = () => {
             <h1 className='text-5xl font-extrabold tracking-tight text-black'>
               Kingdom.
             </h1>
-          </div>
-          <div className='py-12'>
-            <div className='max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
+            <div className='pt-12'>
               <h2 className='sr-only'>For the Kingdom</h2>
               <dl className='space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8'>
                 {features.map((feature) => (
