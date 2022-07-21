837e1aafcc705c62ef4ec50a55a65680909c6d7f
Richard Dubay <richardtdubayjr@gmail.com>
2022-07-21 18:06:10 -0400

    Added Google Global Site Tag and did some cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,25 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["UA-234931034-1"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Google Global Site Tag will not be loaded at all for visitors that have “Do Not Track” enabled.
+          respectDNT: true,
+        },
+      },
+    },
   ],
 };
